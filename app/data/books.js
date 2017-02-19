fdcb298cc95b2d7d446727298b080f7c7db94c34
Sonali Belapurkar <sonashri@gmail.com>
2017-02-19 22:12:47 +0000

    Changes made by Sonali Belapurkar (sonashri@gmail.com).

--- app/data/books.js
+++ app/data/books.js
@@ -1,60 +1,55 @@
 {
-  "books": [
-    {
-        "name": "JavaScript: The Good Parts",
-        "author": "Douglas Crockford",
-        "isbn": "978-0596517748",
-        "amazon_url": "http://www.amazon.com/JavaScript-Good-Parts-Douglas-Crockford/dp/0596517742",
-        "image_url": "img/book_images/goodparts.jpeg",
-        "is_favorite": true
-    },
-    {
-      "name": "JavaScript: The Definitive Guide",
-      "author": "David Flanagan",
-      "isbn": "978-0596805524",
-      "amazon_url": "http://www.amazon.com/JavaScript-Definitive-Guide-Activate-Guides/dp/0596805527",
-      "image_url": "img/book_images/defguide.jpeg",
-      "is_favorite": true
-    },
-    {
-      "name": "The Principles of Object-Oriented JavaScript",
-      "author": "Nicholas C. Zakas",
-      "isbn": "978-1593275402",
-      "amazon_url": "http://www.amazon.com/Principles-Object-Oriented-JavaScript-Nicholas-Zakas/dp/1593275404",
-      "image_url": "img/book_images/oojs.jpeg",
-      "is_favorite": false
-    },
-    {
-      "name": "Eloquent JavaScript: A Modern Introduction to Programming",
-      "author": "Marijn Haverbeke",
-      "isbn": "978-1593275846",
-      "amazon_url": "http://www.amazon.com/Eloquent-JavaScript-Modern-Introduction-Programming/dp/1593275846",
-      "image_url": "img/book_images/eloquent.jpeg",
-      "is_favorite": true
-    },
-    {
-      "name": "Speaking JavaScript",
-      "author": "Axel Rauschmayer",
-      "isbn": "978-1449365035",
-      "amazon_url": "http://www.amazon.com/Speaking-JavaScript-Axel-Rauschmayer-ebook/dp/B00INES03O",
-      "image_url": "img/book_images/speaking.jpeg",
-      "is_favorite": false
-    },
-    {
-      "name": "jQuery UI in Action",
-      "author": "T.J. VanToll",
-      "isbn": "978-1617291937",
-      "amazon_url": "http://www.amazon.com/jQuery-UI-Action-T-VanToll/dp/1617291935",
-      "image_url": "img/book_images/jqueryui.jpeg",
-      "is_favorite": true
-    },
-    {
-      "name": "JavaScript and JQuery: Interactive Front-End Web Development",
-      "author": "Joe Duckett",
-      "isbn": "978-1118531648",
-      "amazon_url": "http://www.amazon.com/gp/product/1118531647",
-      "image_url": "img/book_images/js-jquery.jpeg",
-      "is_favorite": false
-    }
-  ]
+    "Food": [
+        {
+            "name": "Samosa",
+            "author": "Grams of carbs per serving: 15",
+            "isbn": "Serving size: 1 samosa",
+            "image_url": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Vegetable_Samosa.jpg",
+            "is_Indian": true
+        },
+        {
+            "name": "Roti",
+            "author": "Grams of carbs per serving: 15",
+            "isbn": "Serving size: 1 roti",
+            "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Makai_Ki_Roti.JPG/1280px-Makai_Ki_Roti.JPG",
+            "is_Indian": true
+        },
+        {
+            "name": "Puri",
+            "grams_per_serving": "Grams of carbs per serving: 15",
+            "serving_size": "Serving size: 1 puri",
+            "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Dal_Puri.JPG/1280px-Dal_Puri.JPG",
+            "is_Indian": false
+        },
+        {
+            "name": "Naan",
+            "author": "Grams of carbs per serving: 30",
+            "isbn": "Serving size: 1 naan",
+            "image_url": "https://upload.wikimedia.org/wikipedia/commons/4/4e/Annapurna_Naan.jpg",
+            "is_Indian": true
+        },
+        {
+            "name": "Dosa",
+            "author": "Grams of carbs per serving: 15",
+            "isbn": "Serving size: 1 dosa",
+            "image_url": "https://c1.staticflickr.com/5/4063/4656565359_07d7d037d6_b.jpg",
+            "is_Indian": false
+        },
+        {
+            "name": "jQuery UI in Action",
+            "author": "T.J. VanToll",
+            "isbn": "978-1617291937",
+            "amazon_url": "http://www.amazon.com/jQuery-UI-Action-T-VanToll/dp/1617291935",
+            "image_url": "img/book_images/jqueryui.jpeg",
+            "is_Indian": false
+        },
+        {
+            "name": "JavaScript and JQuery: Interactive Front-End Web Development",
+            "author": "Joe Duckett",
+            "isbn": "978-1118531648",
+            "amazon_url": "http://www.amazon.com/gp/product/1118531647",
+            "image_url": "img/book_images/js-jquery.jpeg",
+            "is_Indian": false
+        }
+    ]
 }
